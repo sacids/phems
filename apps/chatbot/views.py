02df20c59ddd6fe570e35a8927745c06eb1f2d64
renfrid <ngolongolo@multics.co.tz>
2022-04-05 10:39:28 +0300

    configure telegram chatbot

--- apps/chatbot/views.py
+++ apps/chatbot/views.py
@@ -11,7 +11,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 TELEGRAM_URL = "https://api.telegram.org/bot"
 BOT_TOKEN = "5210813918:AAEB0WKoRdQ8R3dMPVYXNmnS-4HwhtG8Ov4"
 
-
+#whatsapp bot
 @csrf_exempt
 def whatsapp(request):
     user = request.POST.get('From')
@@ -24,7 +24,8 @@ def whatsapp(request):
 
 
 #telegram bot
-def telegram(self, request):
+@csrf_exempt
+def telegram(request):
     t_data = json.loads(request.body)
     t_message = t_data["message"]
     t_chat = t_message["chat"]
@@ -32,17 +33,17 @@ def telegram(self, request):
     print(f'telegram bot says {t_message}')
 
     msg = "Thank for your message! Sacids Team will be back to you shortly!"
-    # self.send_message(msg, t_chat["id"])
+    send_message(msg, t_chat["id"])
 
     return JsonResponse({"ok": "POST request processed"})
 
 # @staticmethod
-# def send_message(message, chat_id):
-#     data = {
-#         "chat_id": chat_id,
-#         "text": message,
-#         "parse_mode": "Markdown",
-#     }
-#     response = requests.post(
-#             f"{TELEGRAM_URL}{TUTORIAL_BOT_TOKEN}/sendMessage", data=data
-#         )
+def send_message(message, chat_id):
+    data = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "Markdown",
+    }
+    response = requests.post(
+            f"{TELEGRAM_URL}{BOT_TOKEN}/sendMessage", data=data
+        )
