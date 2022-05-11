4c6c1873009dd1674ef0d3bcd25e4237189aa32d
renfrid <ngolongolo@multics.co.tz>
2022-05-11 09:50:34 +0300

    modify gitignore

--- apps/ems/templatetags/filters.py
+++
@@ -1,7 +0,0 @@
-
-from django.template import Library
-register = Library()
-
-@register.filter(name='range')
-def filter_range(start, end):
-    return range(start, end)
--- apps/webpush/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebpushConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.webpush'
