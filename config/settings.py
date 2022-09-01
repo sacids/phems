37ecb7867e7f9d2af61a5f50b7de91f1f08e1796
renfrid <ngolongolo@multics.co.tz>
2022-09-01 14:56:38 +0300

    creation of notification app

--- config/settings.py
+++ config/settings.py
@@ -160,14 +160,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # send email settings
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'renfrid.ngolongolo@gmail.com'
-# EMAIL_HOST_PASSWORD = 'thxzwxglwhtufsbp'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 CORS_ORIGIN_ALLOW_ALL = True
