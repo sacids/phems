80af26c4694470a0c4fc0bd2fe3bf4828634436b
mchizani <ericbeda@gmail.com>
2022-09-01 15:00:41 +0300

    fixed gitignore

--- apps/verifier/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-class VerifierAdmin(admin.ModelAdmin):
-    list_display = ['id','user', 'credibility_score' ,'created_on']
-    
-admin.site.register(Verifier,VerifierAdmin)
--- apps/webpush/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from .views import *
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path("subscription/", subscription, name="subscribe"),
-    path("push/", send_push, name="push"),
-]
