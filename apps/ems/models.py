1f82302df86a063a96e3c502e1aa69fecc136823
renfrid <ngolongolo@multics.co.tz>
2022-10-21 15:49:01 +0300

    Add form in stage

--- apps/ems/models.py
+++ apps/ems/models.py
@@ -120,7 +120,7 @@ class Contact(models.Model):
 
 class Stage(models.Model):
     title           = models.CharField(max_length=255)
-    #form            = models.ForeignKey('Form', on_delete=models.CASCADE,blank=True, null=True)
+    form            = models.ForeignKey('Form', on_delete=models.CASCADE,blank=True, null=True)
 
     def __str__(self):
         return self.title
