18cd9d6606af775e9ba378d8e45709c48b1d6560
mchizani <ericbeda@gmail.com>
2023-04-28 14:16:40 +0300

    update sms log

--- apps/sms/admin.py
+++ apps/sms/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class SMSAdmin(admin.ModelAdmin):
+    list_display = ['msg','msisdn','shortcode','smsc_id','created_on']
+    
+admin.site.register(SmsLog, SMSAdmin)
