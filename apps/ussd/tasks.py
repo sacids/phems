2b0cc1b58155d77a606da982c6012070c6104c9d
mchizani <ericbeda@gmail.com>
2023-05-13 21:23:21 +0300

    minor changes

--- apps/ussd/tasks.py
+++ apps/ussd/tasks.py
@@ -10,7 +10,7 @@ import logging
 def send_response(msg, session_id, req_type):
     xml   = '''
     <?xml version="1.0" encoding="utf-8"?>
-    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"xmlns:xsi="htt p://www.w3.org/2001/XMLSchema- instance"xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
         <soap:Body>
             <InsertMO xmlns="http://tempuri.org/">
                 <user>'''+cfg.halotel['pmo_username']+'''</user>
